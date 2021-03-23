dd814bbd1f3befdb6a275bf043516047fae008e4
SanjayKumarPradhan <sanjay.pradhan@thedatateam.in>
2021-03-23 07:32:11 +0530

    basic router setup

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
+const authRoutes = require("./routes/auth")
+
+//DB Connection
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -11,8 +17,17 @@ mongoose.connect(process.env.DATABASE,{
     console.log("DB CONNECTED")
 })
 
+//middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api",authRoutes)
+
  const port =process.env.PORT || 8000;
 
+ //starting a server
  app.listen(port,()=>{
      console.log(`app is running at ${port}`)
  })
